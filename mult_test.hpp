37d7c3ee01187af5b58ae330419ce35cf7fa4dc2
phamrachel17 <phamrachel17@yahoo.com>
2022-02-16 00:30:17 -0800

    added mult tests

--- mult_test.hpp
+++ mult_test.hpp
@@ -13,6 +13,14 @@ TEST(MultTest, MultEvalNonZero){
   delete op1, op2, res;
 }
 
+TEST(MultTest, MultEvalNeg){
+  Base *op1 = new Op(-8.0);
+  Base *op2 = new Op(4.0);
+  Base *res = new Mult(op1, op2);
+  EXPECT_EQ(res->evaluate(), -32.000000);
+  delete op1, op2, res;
+}
+
 TEST(MultTest, MultEvalZero){
   Base *op1 = new Op(8.0);
   Base *op2 = new Op(0.0);
@@ -42,6 +50,14 @@ TEST(MultTest, MultStringNonZero){
   Base *op2 = new Op(4.0);
   Base *res = new Mult(op1, op2);
   EXPECT_EQ(res->stringify(),"(8.000000*4.000000)");
+  delete op1, op2, res;
+}
+
+TEST(MultTest, MultStringNeg){
+  Base *op1 = new Op(-8.0);
+  Base *op2 = new Op(4.0);
+  Base *res = new Mult(op1, op2);
+  EXPECT_EQ(res->stringify(),"(-8.000000*4.000000)");
   delete op1, op2, res;
 }
 
