2a24882409551affe9d0e38e8693f51a042befc2
phamrachel17 <phamrachel17@yahoo.com>
2022-02-16 01:39:09 -0800

    add div tests

---
+++ div_test.hpp
@@ -0,0 +1,73 @@
+#ifndef __DIV_TEST_HPP__
+#define __DIV_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "div.hpp"
+
+TEST(DivTest, DivEvalNonZero){
+  Base *op1 = new Op(8.0);
+  Base *op2 = new Op(4.0);
+  Base *res = new Div(op1, op2);
+  EXPECT_EQ(res->evaluate(), 2.000000);
+  delete op1, op2, res;
+}
+
+TEST(DivTest, DivEvalNeg){
+  Base *op1 = new Op(-8.0);
+  Base *op2 = new Op(4.0);
+  Base *res = new Div(op1, op2);
+  EXPECT_EQ(res->evaluate(), -2);
+  delete op1, op2, res;
+}
+
+TEST(DivTest, DivEvalZero){
+  Base *op1 = new Op(0.0);
+  Base *op2 = new Op(8.0);
+  Base *res = new Div(op1, op2);
+  EXPECT_EQ(res->evaluate(), 0.000000);
+  delete op1, op2, res;
+}
+
+TEST(DivTest, DivEvalLarge){
+  Base *op1 = new Op(9645.0);
+  Base *op2 = new Op(643.0);
+  Base *res = new Div(op1, op2);
+  EXPECT_EQ(res->evaluate(), 15.000000);
+  delete op1, op2, res;
+}
+
+
+TEST(DivTest, DivStringNonZero){
+  Base *op1 = new Op(8.0);
+  Base *op2 = new Op(4.0);
+  Base *res = new Div(op1, op2);
+  EXPECT_EQ(res->stringify(),"(8.000000/4.000000)");
+  delete op1, op2, res;
+}
+
+TEST(DivTest, DivStringNeg){
+  Base *op1 = new Op(-8.0);
+  Base *op2 = new Op(4.0);
+  Base *res = new Div(op1, op2);
+  EXPECT_EQ(res->stringify(),"(-8.000000/4.000000)");
+  delete op1, op2, res;
+}
+
+TEST(DivTest, DivStringZero){
+  Base *op1 = new Op(8.0);
+  Base *op2 = new Op(0.0);
+  Base *res = new Div(op1, op2);
+  EXPECT_EQ(res->stringify(),"(8.000000/0.000000)");
+  delete op1, op2, res;
+}
+
+TEST(DivTest, DivStringLarge){
+  Base *op1 = new Op(9645.0);
+  Base *op2 = new Op(643.0);
+  Base *res = new Mult(op1, op2);
+  EXPECT_EQ(res->stringify(),"(9645.000000*643.000000)");
+  delete op1, op2, res;
+}
+
+#endif
