6bae59da11c482cf77463cb117b64c6837f8ec63
phamrachel17 <phamrachel17@yahoo.com>
2022-02-17 08:49:07 -0800

    add main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 #include "base.hpp"
+#include "op.hpp"
+#include "mult.hpp"
+#include "div.hpp"
+#include "add.hpp"
+#include "sub.hpp"
+#include "pow.hpp"
+#include "rand.hpp"
 
 int main() {
     // This is a very basic main, and being able to correctly execute this main
@@ -13,7 +20,18 @@ int main() {
     Base* mult = new Mult(seven, four);
     Base* add = new Add(three, mult);
     Base* minus = new Sub(add, two);
+    Base* pow = new Pow(seven, two);
+    Base* div = new Div(minus, two);
+    Base* rand = new Rand();
 
+
+    std::cout << seven->stringify() << " = " << seven->evaluate() << std::endl;
     std::cout << minus->stringify() << " = " << minus->evaluate() << std::endl;
+    std::cout << mult->stringify() << " = " << mult->evaluate() << std::endl;
+    std::cout << add->stringify() << " = " << add->evaluate() << std::endl;
+    std::cout << pow->stringify() << " = " << pow->evaluate() << std::endl;
+    std::cout << div->stringify() << " = " << div->evaluate() << std::endl;
+    std::cout << rand->stringify() << " = " << rand->evaluate() << std::endl;
+
     return 0;
 }
